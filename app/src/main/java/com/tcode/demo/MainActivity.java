c96f5047a85160f2fccfa205b5e35cfa6ad28944
tyc874000221 <874000221@qq.com>
2017-06-28 15:31:45 +0800

    add content

--- app/src/main/java/com/tcode/demo/MainActivity.java
+++ app/src/main/java/com/tcode/demo/MainActivity.java
@@ -1,20 +1,128 @@
 package com.tcode.demo;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.tcode.demo.adapter.MainAdapter;
+import com.tcode.demo.bean.ListData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivity mContext;
 
+    private RecyclerView mainRecyclerView;
+    private SwipeRefreshLayout mainSwipeRefreshLayout;
+    private LinearLayoutManager mLinearLayoutManager;//刷新和加载更多的管理
+    private LinearLayout pgb_bottom;
+    private List<ListData> list;
+    private Handler handler = new Handler();
+    private MainAdapter mainAdapter;
+
+    int k =1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();//关联控件
+        initData();//初始化数据
+        initView();//初始化视图
+
+    }
+
+    private void init()
+    {
+        mContext = this;
+        mainRecyclerView = (RecyclerView)findViewById(R.id.mainRecyclerView);
+        mainSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.mainSwipeRefreshLayout);
+        pgb_bottom = (LinearLayout)findViewById(R.id.pgb_bottom);
+        mLinearLayoutManager = new LinearLayoutManager(mContext);
+    }
+
+    private void initData()
+    {
+        list = new ArrayList<>();
+        for (int i = 0;i < 5 ;i ++)
+        {
+            ListData listData = new ListData("group-1  item-",i);
+            list.add(listData);
+        }
+    }
+
+    private void initView()
+    {
+        mainAdapter = new MainAdapter(mContext,list);
+        mainRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mainRecyclerView.setAdapter(mainAdapter);
+        mainRecyclerView.addItemDecoration(new DividerItemDecoration(mContext,DividerItemDecoration.VERTICAL));
 
 
+        //下拉刷新
+        mainSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                mainSwipeRefreshLayout.setRefreshing(true);
+                moreData();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mainAdapter.notifyDataSetChanged();
+                        mainSwipeRefreshLayout.setRefreshing(false);//停止刷新动画
+                    }
+                }, 1000);
+
+            }
+        });
 
 
+        //上拉加载
+        mainRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                //判断是否滑动到底部
+                if (!recyclerView.canScrollVertically(1)) {
+                    pgb_bottom.setVisibility(View.VISIBLE);
+                    moreData();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            pgb_bottom.setVisibility(View.INVISIBLE);
+                            mainAdapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                }
+            }
+        });
+    }
+
+
+    //更所数据
+    private void moreData()
+    {
+        k++;
+        for (int i = 0;i < 9 ;i ++)
+        {
+            ListData listData = new ListData("group-"+k+"  item",i);
+            list.add(listData);
+        }
     }
 
 
--- app/src/main/java/com/tcode/demo/adapter/MainAdapter.java
+++ app/src/main/java/com/tcode/demo/adapter/MainAdapter.java
@@ -2,6 +2,7 @@ package com.tcode.demo.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.RecyclerView.ViewHolder;
@@ -11,19 +12,10 @@ import com.tcode.demo.R;
 import com.tcode.demo.bean.ListData;
 import java.util.List;
 
-/**
- * 1.因为要添加底部的加载状态，所以返回的数据要多处一列放置底部的布局
- * 2.因为底部最一行的布局和别的正常数据的布局不一样，所以要根据返回不同的视图类型给予不同的布局文件
- *  （如果不需要上拉加载更多，则不需要重写此方法）
- * 3.如果数据没有10条（这里的数据每页显示十条，根据每页显示的条数来给定具体的数值）则最后一行不显示加载更多的进度条
- */
 public class MainAdapter extends RecyclerView.Adapter<ViewHolder> {
 
-	private int TYPE_ITEM = 1;//普通数据列
-	private int TYPE_FOOT = 0;//底部加载列
-
 	private Context mContext;
-	private List<ListData> list;//数据列表
+	private List<ListData> list;
 
 
 	public MainAdapter (Context mContext,List<ListData> list){
@@ -31,64 +23,38 @@ public class MainAdapter extends RecyclerView.Adapter<ViewHolder> {
 		this.list = list;
 	}
 
-
-
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
+		MViewHolder viewHolder = new MViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_main_rv, parent, false));
+		return  viewHolder;
 
-
-		return null;
 	}
 
 	@Override
 	public void onBindViewHolder(ViewHolder holder, int position) {
 
-
-
-
+		if(holder instanceof MViewHolder){
+			((MViewHolder) holder).tv_dataName.setText(list.get(position).getDataName());
+			((MViewHolder) holder).tv_dataNum.setText(String.valueOf(list.get(position).getDataNum()));
+		}
 
 	}
 
 	@Override
 	public int getItemCount() {
-		return list.size() == 0 ? 0 : list.size()+1;//说明1
+		return list.size() == 0 ? 0 : list.size();
 	}
-
-	//说明2
-	@Override
-	public int getItemViewType(int position) {
-		//说明3
-		if(getItemCount() < 10){
-			return TYPE_ITEM;
-		}
-
-		if (position + 1 == getItemCount()){
-			return TYPE_FOOT;
-		}else{
-			return TYPE_ITEM;
-		}
-
-	}
-
 
 	class MViewHolder extends ViewHolder{
 
-		TextView tv_dataName;
+		TextView tv_dataName,tv_dataNum;
 
 		public MViewHolder(View itemView) {
 			super(itemView);
-
-//			tv_dataName = (TextView)itemView.findViewById(R.id.tv_dataName);
+			tv_dataName = (TextView)itemView.findViewById(R.id.tv_dataName);
+			tv_dataNum = (TextView)itemView.findViewById(R.id.tv_dataNum);
 		}
-
-
-
 	}
-
-
-
-
-
 
 }
